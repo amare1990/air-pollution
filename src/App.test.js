c0c6fabb7f8c7bc85c5e684ba6dc6ba85d3745e1
amare1990 <amaremek@gmail.com>
2022-12-03 21:28:06 +0300

    Adjust the APP and Index component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
+import store from './redux/store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Testing if App component renders correctly', () => {
+  const appSnapshot = renderer.create(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+  ).toJSON();
+  expect(appSnapshot).toMatchSnapshot();
 });
